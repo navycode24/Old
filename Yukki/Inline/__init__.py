8b29902a2f4b8af9104d0e68c72ad3ce1849c94f
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-02-13 20:30:52 +0700

    update __init__.py

--- Yukki/Inline/__init__.py
+++ Yukki/Inline/__init__.py
@@ -8,7 +8,6 @@ from .playlist import (add_genre_markup, check_genre_markup, check_markup,
                        paste_queue_markup, play_genre_playlist,
                        playlist_markup, third_playlist_markup)
 from .song import song_download_markup, song_markup
-from .start import (custommarkup, dashmarkup, private_panel, setting_markup,
-                    setting_markup2, start_pannel, usermarkup, volmarkup)
+from .start import *
 from .stats import stats1, stats2, stats3, stats4, stats5, stats6, stats7
 from .videocalls import choose_markup, livestream_markup, stream_quality_markup
