9ac7b429980cefc44697f5c368a9d76e5a918dc8
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-02-13 20:37:57 +0700

    update start.py

--- Yukki/Inline/start.py
+++ Yukki/Inline/start.py
@@ -1,7 +1,7 @@
 from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup, InputMediaPhoto, Message)
 
-from config import MUSIC_BOT_NAME, SUPPORT_CHANNEL, SUPPORT__GROUP
+from config import MUSIC_BOT_NAME, SUPPORT_CHANNEL, SUPPORT_GROUP
 from Yukki import BOT_USERNAME
 
 
