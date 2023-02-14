32030ba4f6d7c30ca08c18e76471802e3e95ccd5
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-02-14 14:09:17 +0700

    update Start.py

--- Yukki/Plugins/Start.py
+++ Yukki/Plugins/Start.py
@@ -7,7 +7,7 @@ from pyrogram.types import (InlineKeyboardButton,
                             InlineKeyboardMarkup,Message)
 
 from Yukki import *
-from Yukki import *
+from Yukki.Utilities.ytdl import ytdl_opts
 from Yukki.Core.PyTgCalls import Yukki
 from Yukki.Database import *
 from Yukki.Decorators.admins import ActualAdminCB
